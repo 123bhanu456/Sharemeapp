758b59b3c038ed93c71616c963f298d0e3b9fab6
123bhanu456 <kalahastribahnuprakash@gmail.com>
2023-06-10 11:05:10 +0530

    feat : Schemas

---
+++ Backend/schemas/index.ts
@@ -0,0 +1,8 @@
+import user from './User'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './Postedby'
+import save from './save'
+
+
+export const schemaTypes = [user,pin,save,comment,postedBy]
